24b6477b08780f5e87d90dc28355b57f55cd5735
dyndtikj <vlamaster08@gmail.com>
2022-10-23 21:40:45 +0300

    Fixing valgrind

--- scripts/valgrind.sh
+++ scripts/valgrind.sh
@@ -7,7 +7,7 @@ VG_LOG="./build/valgrind.log"
 NO_LOST_PATTERN="All heap blocks were freed -- no leaks are possible"
 NO_ERROR_PATTERN="ERROR SUMMARY: 0 errors"
 
-rm -f VALGRIND_LOG
+rm -f VG_LOG
 
 valgrind --tool=memcheck --leak-check=summary --leak-check=full --track-origins=yes --log-file=${VG_LOG} ${TEST_FILES} || exit 1
 NO_LOST=$(grep "${NO_LOST_PATTERN}" "${VG_LOG}")
