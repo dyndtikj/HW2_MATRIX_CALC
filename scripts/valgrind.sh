0adcc99beaa922e06001d42fbf5601d7626a526c
dyndtikj <vlamaster08@gmail.com>
2022-10-23 22:07:56 +0300

    Fixing valgrind

--- scripts/valgrind.sh
+++ scripts/valgrind.sh
@@ -4,13 +4,15 @@ TEST_FILES="./build/tests/tests"
 
 VG_LOG="./build/valgrind.log"
 
+chmod a+rwx ${TEST_FILES}
+
 NO_LOST_PATTERN="All heap blocks were freed -- no leaks are possible"
 NO_ERROR_PATTERN="ERROR SUMMARY: 0 errors"
 
 rm -f VG_LOG
 
 valgrind --tool=memcheck --leak-check=summary --leak-check=full --track-origins=yes --log-file=${VG_LOG} ${TEST_FILES} || exit 1
-#NO_LOST=$(grep "${NO_LOST_PATTERN}" "${VG_LOG}")
-#NO_ERROR=$(grep "${NO_ERROR_PATTERN}" "${VG_LOG}")
-#if [ -z "${NO_LOST}" ] || [ -z "${NO_ERROR}" ]; then cat "${VG_LOG}"; exit 1; fi
-#echo " ***** SUCCESS MEMTEST *****"
+NO_LOST=$(grep "${NO_LOST_PATTERN}" "${VG_LOG}")
+NO_ERROR=$(grep "${NO_ERROR_PATTERN}" "${VG_LOG}")
+if [ -z "${NO_LOST}" ] || [ -z "${NO_ERROR}" ]; then cat "${VG_LOG}"; exit 1; fi
+echo " ***** SUCCESS MEMTEST *****"
